5ca8bc19babf463dc8434e34f9c632c574088648
Deavaly <27pfch8bgq@privaterelay.appleid.com>
2026-05-02 15:30:12 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -43,15 +43,16 @@ module.exports = async (req, res) => {
       },
       body: JSON.stringify({
         // СЕКРЕТНОЕ ОРУЖИЕ: Если базовая Llama 3.1 все еще тупит, поменяй строку ниже на:
-        // model: 'nousresearch/hermes-3-llama-3.1-8b',
-        model: 'meta-llama/llama-3.1-8b-instruct',
+        // model: 'nousresearch/hermes-3-llama-3.1-8b', 'meta-llama/llama-3.1-8b-instruct',
+        model: 'mistralai/mistral-7b-instruct:free',
         messages: [
           { role: 'system', content: systemPrompt },
           ...messages
         ],
-        temperature: 0.85, // Подняли с 0.6 до 0.85, чтобы убрать шаблонность бота
+        temperature: 0.5, // Подняли с 0.6 до 0.85, чтобы убрать шаблонность бота
         top_p: 0.9,       // Добавлено для естественности речи
-        frequency_penalty: 0.3 // Заставит её использовать более разнообразные слова, уберет зацикленность
+        max_tokens: 500      // Чтобы она не уходила в бесконечные рассуждения
+        frequency_penalty: 0.5 // Заставит её использовать более разнообразные слова, уберет зацикленность
       })
     });
 
